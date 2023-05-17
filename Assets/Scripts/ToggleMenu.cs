5b4c93dc241de61c4c8c66e4551070f1f42b540d
polyqubit <polyumbrial@gmail.com>
2023-05-16 22:31:57 -0700

    successful creation of prefab button

--- Assets/Scripts/ToggleMenu.cs
+++ Assets/Scripts/ToggleMenu.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ToggleMenu : MonoBehaviour
 {
+    public GameObject button;
     private bool active = false;
     // Start is called before the first frame update
     void Start()
@@ -15,15 +16,18 @@ public class ToggleMenu : MonoBehaviour
     {
         if (!active)
         {
-            GameObject button = UnityEngine.UI.DefaultControls.CreateButton(new UnityEngine.UI.DefaultControls.Resources());
-            button.transform.position = new Vector3(50, 50, 0);
-            button.transform.name = "bobberton";
-            button.transform.SetParent(transform);
+            GameObject b = Instantiate(button);
+            b.transform.SetParent(transform);
+            b.GetComponent<RectTransform>().anchoredPosition3D = new Vector2(260, -40);
+            b.GetComponent<RectTransform>().anchorMin = new Vector2(0, 1);
+            b.GetComponent<RectTransform>().anchorMax = new Vector2(0, 1);
+            b.GetComponent<RectTransform>().pivot = new Vector2(0, 1);
+            b.GetComponent<RectTransform>().localScale = new Vector2(2, 2);
             active = true;
         }
         else 
         {
-            Destroy(transform.Find("bobberton").gameObject);
+            Destroy(transform.Find("digbutton(Clone)").gameObject);
             active = false;
         }
         
