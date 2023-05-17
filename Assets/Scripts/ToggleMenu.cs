c151e9d467f43c1fd6c525b6e82bc98447b183b4
polyqubit <polyumbrial@gmail.com>
2023-05-16 22:08:58 -0700

    creation and destruction of button through code
    but perhaps transition to prefabs?

--- Assets/Scripts/ToggleMenu.cs
+++ Assets/Scripts/ToggleMenu.cs
@@ -4,9 +4,28 @@ using UnityEngine;
 
 public class ToggleMenu : MonoBehaviour
 {
+    private bool active = false;
     // Start is called before the first frame update
     void Start()
     {
+        
+    }
+
+    public void ToggleButton()
+    {
+        if (!active)
+        {
+            GameObject button = UnityEngine.UI.DefaultControls.CreateButton(new UnityEngine.UI.DefaultControls.Resources());
+            button.transform.position = new Vector3(50, 50, 0);
+            button.transform.name = "bobberton";
+            button.transform.SetParent(transform);
+            active = true;
+        }
+        else 
+        {
+            Destroy(transform.Find("bobberton").gameObject);
+            active = false;
+        }
         
     }
 
