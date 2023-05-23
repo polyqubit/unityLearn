ae959681cbf6a0acbb1d120fdd10231871e7ac1d
polyqubit <polyumbrial@gmail.com>
2023-05-23 10:13:47 -0700

    aura

--- Assets/Scripts/DigMode.cs
+++ Assets/Scripts/DigMode.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class DigMode : MonoBehaviour
 {
+    public GameObject locate;
     private bool dmode = false;
     // Start is called before the first frame update
     void Start()
@@ -20,12 +21,13 @@ public class DigMode : MonoBehaviour
 
     void DisplayBreakableTiles()
     {
-        Vector3 pos = transform.position;
-        Collider2D right = Physics2D.OverlapCircle(new Vector2(pos.x + 1, pos.y), 0.2f, LayerMask.GetMask("ground"));
-        if (right is not null)
-        {
-            Debug.Log(right);
-        }
+        //Vector3 pos = transform.position;
+        //Collider2D right = Physics2D.OverlapCircle(new Vector2(pos.x + 1, pos.y), 0.2f, LayerMask.GetMask("ground"));
+        //if (right is not null)
+        //{
+        //    Debug.Log(right);
+        //}
+        Instantiate(locate, transform); //.transform.position = new Vector2(-1, 0);
     }
 
     // Update is called once per frame
